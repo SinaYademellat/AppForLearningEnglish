468d839f3338febcc5c83ba9429eb4e4afa31edf
シ <65041880+SinaYademellat@users.noreply.github.com>
2025-04-13 01:16:22 +0330

    how use class

--- Lernkartei_data_structure.py
+++ Lernkartei_data_structure.py
@@ -1,10 +1,19 @@
 """_summary_
- این این قسمت یک فیلم یوتیوب هست که یاد داد
+ لغت های همان روز را داریم 
+ لعت های دیروز
+ و 
+ ...
 
+ اگر هم جایی اشتباه کریم باید بیام همان اول کار
+
+ دیتاست ها هم به شکل پیل ذخیره می کنیم
 """
 
 class Lernkartei_word:
-    def __init__(self, Txt:str  , Status:int , Collocations:str = None ) -> None:
+    """
+     سلام 
+    """
+    def __init__(self, Txt:str  ,  Collocations:str = None , Status:int=None  ) -> None:
         """_summary_
 
         Args:
@@ -20,9 +29,8 @@ class Lernkartei_word:
         """
         self.txt = Txt
         self.collocations = Collocations
-        # self.farsi = Farsi
         self.status = Status 
-
+        
     def Save_in_txt(self):
         main_path = 'tmp_db_txt/'
         txt_file =f'Status_{self.status}.txt'
@@ -37,8 +45,20 @@ class Lernkartei_word:
         f.write(self.collocations + '\n')
         f.close()
 
-if __name__ == '__main__':
-    A = Lernkartei_word(Txt='test_1', Status=2,Collocations='colloca_1')
-    A.Save_in_txt()
-    A.txt ='test_2'
-    A.Save_in_txt()
+
+    def __repr__(self):
+        my_print = f"""
+                Txt = {self.txt}
+                col = {self.collocations}
+                sta = {self.status}
+            """
+        
+        return my_print
+        
+
+
+# if __name__ == '__main__':
+#     A = Lernkartei_word(Txt='test_1', Status=2,Collocations='colloca_1')
+#     A.Save_in_txt()
+#     A.txt ='test_2'
+#     A.Save_in_txt()
