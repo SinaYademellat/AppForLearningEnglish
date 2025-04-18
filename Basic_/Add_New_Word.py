0b9aacbb333c30b013e007cc6bb1419fd00eb49c
シ <65041880+SinaYademellat@users.noreply.github.com>
2025-04-18 14:28:21 +0330

    gui --> word

--- Basic_/Add_New_Word.py
+++ Basic_/Add_New_Word.py
@@ -1,9 +1,15 @@
+import os
+import pickle
 import tkinter as tk
 from Lernkartei_data_structure import Lernkartei_word
 
+
+
+
+
 class AddNewWord_app():
     def __init__(self) -> None:
-        self.counter_nuber_is = 0
+        # self.counter_nuber_is = 0
         self.newWordList = []
         self.root_of_AddNewWord_app = tk.Tk()
         # . . . . . . . . . . . . . . . . . . . . . .
@@ -20,12 +26,10 @@ class AddNewWord_app():
         self.root_of_AddNewWord_app.geometry(f"{window_width}x{window_height}+{ x_offset }+{y_offset}")
         self.root_of_AddNewWord_app.resizable(False,False)
         # ----------
+        # self.root_of_AddNewWord_app.bind('<Return>', self.GG)
 
-
-    def GG(self,event):
-        self.counter_nuber_is+=1
-        self.label_counter["text"] = f'{self.counter_nuber_is}'
-
+    def show_len_newWordList_notsave(self):
+        self.label_counter["text"] = f'{len(self.newWordList)}'
 
     def set_counter(self,possitain_XY:tuple =(0,89)):
         self.label_counter = tk.Label(self.root_of_AddNewWord_app,
@@ -37,12 +41,40 @@ class AddNewWord_app():
                                 )
         self.label_counter.place(x = possitain_XY[0],y=possitain_XY[1])
 
-        self.root_of_AddNewWord_app.bind('<Return>', self.GG)
 
+    def clean_Entry(self):
+        self.e1_text.delete(0, 'end')
+        self.e2_collocations.delete(0, 'end')
 
-    def FF(self):
-        print('s')
-   
+    def Gui_to_local_list_MyDatastructure(self)->None:
+        tmp_Word = Lernkartei_word(
+            Txt=self.e1_text.get() ,
+            Collocations= self.e2_collocations.get(),
+            Status=1 )
+        self.newWordList.append(tmp_Word)
+        self.show_len_newWordList_notsave()
+        self.clean_Entry()
+
+    def save_newWordToPkl(self):
+        if(len(self.newWordList)==0): # مثل این که اول کامیت میکنیم بعد پوش میکنیم تو گیت :)
+            print('first push buttom "ok"')
+            return
+        ds_path = '../Data Base'
+        mypath = os.path.join(ds_path, 'Box_1.pkl')
+        with open(mypath, 'rb') as inp:
+            tmp_list_is = pickle.load(inp)
+
+        for i in self.newWordList:
+            tmp_list_is.append(i)
+
+        print('Len Box1: ',len(tmp_list_is))
+        with open(mypath, 'wb') as outp:
+                pickle.dump(tmp_list_is, outp, pickle.HIGHEST_PROTOCOL)
+        self.newWordList = []
+        self.show_len_newWordList_notsave()
+        print('save it')
+  
+  
     def set_Gui(self):
         # -----------------
         backgroundColor_root = '#FF9A9A'
@@ -58,21 +90,22 @@ class AddNewWord_app():
         l1.place(x =0 ,y = 5 + 14)
         l2.place(x =0 ,y = 40 + 14)
 
-        e1 = tk.Entry(self.root_of_AddNewWord_app,font=("Helvetica", 15))
-        e2 = tk.Entry(self.root_of_AddNewWord_app,font=("Helvetica", 15))
-        e1.place(x=120, y = 5 + 14)
-        e2.place(x=120, y = 40 + 14)
+        self.e1_text = tk.Entry(self.root_of_AddNewWord_app,font=("Helvetica", 15))
+        self.e2_collocations = tk.Entry(self.root_of_AddNewWord_app,font=("Helvetica", 15))
+        self.e1_text.place(x=120, y = 5 + 14)
+        self.e2_collocations.place(x=120, y = 40 + 14)
 
         # ! ============================================ 
         self.set_counter()
 
-
-        b1 = tk.Button(self.root_of_AddNewWord_app, width=5, text='ok',font=("Helvetica", 8) , command=self.FF)
+        b1 = tk.Button(self.root_of_AddNewWord_app, width=5, text='ok',font=("Helvetica", 8) , command=self.Gui_to_local_list_MyDatastructure)
         b1.place(x=50,y=88)
 
-
+        b2_saveToPklFile = tk.Button(self.root_of_AddNewWord_app,width=5,text='Save!',font=("Helvetica", 8) ,  command=self.save_newWordToPkl)
+        b2_saveToPklFile.place(x=180 , y=88)
+        
 
 
 if (__name__=='__main__'):
     AddNewWord_app()
-    print()
+    
